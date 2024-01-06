5427406128383fe5f74b482c9de16414dabfd3c3
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-01-06 21:23:25 +0700

    Basic object things

--- gobjectpp/include/gobjectpp/object.hpp
+++ gobjectpp/include/gobjectpp/object.hpp
@@ -1,8 +1,35 @@
 #pragma once
+#include <map>
+#include <string>
+#include <any>
+#include <stdexcept>
 
 namespace GObject
 {
     class Object
     {
+    private:
+        std::map<std::string, std::any> properties;
+
+    public:
+        Object(std::map<std::string, std::any>);
+
+        template <typename T>
+        T get_property(std::string name)
+        {
+            if (this->properties.find(name) == this->properties.end())
+            {
+                throw std::invalid_argument("Property `" + name + "` not found");
+            }
+            return std::any_cast<T>(this->properties[name]);
+        }
+        void set_property(std::string name, std::any value)
+        {
+            if (this->properties.find(name) == this->properties.end())
+            {
+                throw std::invalid_argument("Property `" + name + "` not found");
+            }
+            this->properties[name] = value;
+        }
     };
 }
--- gobjectpp/src/object.cpp
+++ gobjectpp/src/object.cpp
@@ -1,1 +1,8 @@
 #include <gobjectpp/object.hpp>
+
+using namespace GObject;
+
+Object::Object(std::map<std::string, std::any> properties)
+{
+    this->properties = properties;
+}
