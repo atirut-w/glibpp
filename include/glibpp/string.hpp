d5c01b4293fafefa3d1b8d25f68053abe4304d2a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-03-31 18:44:53 +0700

    Implement basic String constructor

--- include/glibpp/string.hpp
+++ include/glibpp/string.hpp
@@ -1,8 +1,16 @@
 #pragma once
+#include <stddef.h>
 
 namespace GLib
 {
     class String
     {
+    private:
+        char *str = nullptr;
+        size_t len = 0;
+        size_t allocated = 0;
+    public:
+        String() = default;
+        String(const char *str);
     };
 }
--- src/string.cpp
+++ src/string.cpp
@@ -1,1 +1,18 @@
 #include <glibpp/string.hpp>
+#include <cstring>
+
+#define ALLOCATE_STEP 128
+
+using namespace GLib;
+
+String::String(const char *init)
+{
+    len = strlen(init);
+    while (allocated < len + 1)
+    {
+        allocated += ALLOCATE_STEP;
+    }
+
+    str = new char[allocated];
+    memcpy(str, init, len + 1);
+}
