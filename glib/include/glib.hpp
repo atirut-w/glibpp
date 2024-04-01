19680b921806f23942bd5121db3c6c95e3ae7f7b
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-04-01 14:01:30 +0700

    Organize file hierarchy

--- glib/include/glib.hpp
+++ glib/include/glib.hpp
@@ -1,24 +1,2 @@
 #pragma once
-
-typedef char gchar;
-typedef unsigned long gsize;
-typedef unsigned char guint8;
-
-class string
-{
-private:
-    gchar *str = nullptr;
-    gsize len = 0;
-    gsize allocated_len = 0;
-
-public:
-    string() = default;
-    string(const char *str);
-    string(const string &other);
-    ~string();
-
-    string &operator=(const string &other);
-
-    const guint8 *get_data() const;
-    gsize get_length() const;
-};
+#include <glib/global.hpp>
