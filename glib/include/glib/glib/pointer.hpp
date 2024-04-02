be208d108a5192ff8fdd175b12a18f54539e8637
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2024-04-02 18:06:15 +0700

    Make `owned` ref-counted

--- glib/include/glib/glib/pointer.hpp
+++ glib/include/glib/glib/pointer.hpp
@@ -3,110 +3,70 @@
 
 namespace GLib
 {
-    // // Encapsulates unowned pointer
-    // template <typename T>
-    // class unowned
-    // {
-    // private:
-    //     T *ptr = nullptr;
-    //     gint *refs = nullptr;
-
-    //     void cleanup()
-    //     {
-    //         if (ptr != nullptr && --(*refs) == 0)
-    //         {
-    //             delete ptr;
-    //             delete refs;
-    //             ptr = nullptr;
-    //             refs = nullptr;
-    //         }
-    //     }
-        
-    // public:
-    //     unowned() : ptr(nullptr), refs(nullptr) {}
-    //     unowned(T *ptr)
-    //     {
-    //         this->ptr = ptr;
-    //         if (ptr != nullptr)
-    //             refs = new gint(1);
-    //     }
-
-    //     unowned(const unowned &other)
-    //     {
-    //         ptr = other.ptr;
-    //         refs = other.refs;
-    //         if (ptr != nullptr)
-    //             (*refs)++;
-    //     }
-
-    //     unowned &operator=(const unowned &other)
-    //     {
-    //         if (this != &other)
-    //         {
-    //             cleanup();
-    //             ptr = other.ptr;
-    //             refs = other.refs;
-    //             if (ptr != nullptr)
-    //                 (*refs)++;
-    //         }
-    //         return *this;
-    //     }
-
-    //     unowned(unowned &&other)
-    //     {
-    //         ptr = other.ptr;
-    //         refs = other.refs;
-    //         other.ptr = nullptr;
-    //         other.refs = nullptr;
-    //     }
-
-    //     void operator=(unowned &&other)
-    //     {
-    //         cleanup();
-    //         ptr = other.ptr;
-    //         refs = other.refs;
-    //         other.ptr = nullptr;
-    //         other.refs = nullptr;
-    //     }
-
-    //     ~unowned() { cleanup(); }
-
-    //     operator gboolean() const { return ptr != nullptr; }
-    //     T *operator->() const { return ptr; }
-    //     T &operator*() const { return *ptr; }
-    // };
-    
-    // Encapsulates owned pointer
+    // Encapsulates owned (ref-counted) pointer
     template <typename T>
     class owned
     {
     private:
         T *ptr = nullptr;
+        gint *refs = nullptr;
 
         void cleanup()
         {
-            if (ptr != nullptr)
+            if (ptr != nullptr && --(*refs) == 0)
+            {
                 delete ptr;
+                delete refs;
+                ptr = nullptr;
+                refs = nullptr;
+            }
+        }
+        
+    public:
+        owned() : ptr(nullptr), refs(nullptr) {}
+        owned(T *ptr)
+        {
+            this->ptr = ptr;
+            if (ptr != nullptr)
+                refs = new gint(1);
         }
 
-    public:
-        owned() = default;
-        owned(T *ptr) : ptr(ptr) {}
+        owned(const owned &other)
+        {
+            ptr = other.ptr;
+            refs = other.refs;
+            if (ptr != nullptr)
+                (*refs)++;
+        }
 
-        owned(const owned &) = delete;
-        owned &operator=(const owned &) = delete;
+        owned &operator=(const owned &other)
+        {
+            if (this != &other)
+            {
+                cleanup();
+                ptr = other.ptr;
+                refs = other.refs;
+                if (ptr != nullptr)
+                    (*refs)++;
+            }
+            return *this;
+        }
 
         owned(owned &&other)
         {
             ptr = other.ptr;
+            refs = other.refs;
             other.ptr = nullptr;
+            other.refs = nullptr;
         }
 
         void operator=(owned &&other)
         {
             cleanup();
             ptr = other.ptr;
+            refs = other.refs;
             other.ptr = nullptr;
+            other.refs = nullptr;
         }
 
         ~owned() { cleanup(); }
