2e6d331bf3c5278c25824998f51c580534b1a40f
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-06-18 19:09:07 +0700

    Implement String constructor and destructor

--- glib/include/glibpp/string.hpp
+++ glib/include/glibpp/string.hpp
@@ -1,7 +1,20 @@
 #pragma once
+#include <cstddef>
 
 namespace GLib {
 
-class String {};
+class String {
+  char *str;
+  std::size_t len;
+  std::size_t allocated_len;
+
+public:
+  String(const char *init);
+  ~String();
+
+  // TODO: Implement copy and assignment
+  String(const String &other) = delete;
+  String &operator=(const String &other) = delete;
+};
 
 } // namespace GLib
--- glib/src/string.cpp
+++ glib/src/string.cpp
@@ -1,1 +1,22 @@
+#include <cstring>
 #include <glibpp/string.hpp>
+
+namespace GLib {
+
+String::String(const char *init) {
+  if (init == nullptr || *init == '\0') {
+    str = new char[1];
+    str[0] = '\0';
+    len = 0;
+    allocated_len = 1;
+  } else {
+    len = std::strlen(init);
+    allocated_len = len + 1; // +1 for null terminator
+    str = new char[allocated_len];
+    std::strcpy(str, init);
+  }
+}
+
+String::~String() { delete[] str; }
+
+} // namespace GLib
