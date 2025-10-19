b05dc706160e4f2e8d921cb95d507c9da09e3e54
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-10-19 23:37:48 +0700

    Flesh out base atomic and atomic ints

--- glib/include/glibpp/atomic.hpp
+++ glib/include/glibpp/atomic.hpp
@@ -2,7 +2,16 @@
 
 namespace GLib {
 
-template <typename T> class Atomic {};
+template <typename T> class Atomic {
+  T value;
+
+public:
+  Atomic() : value() {}
+  Atomic(T v) : value(v) {}
+
+  T get() const { return __atomic_load_n(&value, __ATOMIC_SEQ_CST); }
+  void set(T v) { __atomic_store_n(&value, v, __ATOMIC_SEQ_CST); }
+};
 
 template <> class Atomic<int> {
   int value;
@@ -10,6 +19,13 @@ template <> class Atomic<int> {
 public:
   Atomic() : value(0) {}
   Atomic(int v) : value(v) {}
+
+  int get() const { return __atomic_load_n(&value, __ATOMIC_SEQ_CST); }
+  void set(int v) { __atomic_store_n(&value, v, __ATOMIC_SEQ_CST); }
+  void inc() { __atomic_fetch_add(&value, 1, __ATOMIC_SEQ_CST); }
+  bool dec_and_test() {
+    return __atomic_sub_fetch(&value, 1, __ATOMIC_SEQ_CST) == 0;
+  }
 };
 
 template <typename T> class Atomic<T *> {
