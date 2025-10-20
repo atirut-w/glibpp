78c974b5e5739895cda0d7574bb168f430c9702d
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-10-20 14:34:30 +0700

    Refine refcounted box internals

--- glib/include/glibpp/rcbox.hpp
+++ glib/include/glibpp/rcbox.hpp
@@ -5,118 +5,150 @@
 namespace GLib {
 
 template <typename T> class RcBox {
-  RefCount *ref_count = nullptr;
-  T *data = nullptr;
+  struct Control {
+    RefCount ref_count;
+    T *data = nullptr;
+  };
+
+  Control *control = nullptr;
 
 public:
   RcBox() = default;
 
   RcBox(const T &value) {
-    ref_count = new RefCount();
-    data = new T(value);
+    control = new Control();
+    control->data = new T(value);
   }
 
   RcBox(const RcBox &other) {
-    ref_count = other.ref_count;
-    data = other.data;
-    if (ref_count) {
-      ref_count->inc();
+    control = other.control;
+    if (control) {
+      control->ref_count.inc();
     }
   }
 
   RcBox &operator=(const RcBox &other) {
     if (this != &other) {
-      if (ref_count && ref_count->dec()) {
-        delete data;
-        delete ref_count;
+      if (control && control->ref_count.dec()) {
+        delete control->data;
+        delete control;
       }
-      ref_count = other.ref_count;
-      data = other.data;
-      if (ref_count) {
-        ref_count->inc();
+      control = other.control;
+      if (control) {
+        control->ref_count.inc();
       }
     }
     return *this;
   }
 
   ~RcBox() {
-    if (ref_count && ref_count->dec()) {
-      delete data;
-      delete ref_count;
+    if (control && control->ref_count.dec()) {
+      delete control->data;
+      delete control;
     }
   }
 
   RcBox dup() const {
     RcBox copy;
-    copy.ref_count = new RefCount();
-    copy.data = new T(*data);
+    copy.control = new Control();
+    if (control && control->data) {
+      copy.control->data = new T(*control->data);
+    }
     return copy;
   }
 
-  T &operator*() {
-    return *data;
+  T *steal() {
+    if (!control) {
+      return nullptr;
+    }
+    T *data = control->data;
+    control->data = nullptr;
+    if (control->ref_count.dec()) {
+      delete control;
+    }
+    control = nullptr;
+    return data;
   }
 
-  T *operator->() {
-    return data;
+  operator T*() {
+    if (control) {
+      return control->data;
+    }
+    return nullptr;
   }
 };
 
 template <typename T> class AtomicRcBox {
-  AtomicRefCount *ref_count = nullptr;
-  T *data = nullptr;
+  struct Control {
+    AtomicRefCount ref_count;
+    T *data = nullptr;
+  };
+
+  Control *control = nullptr;
 
 public:
   AtomicRcBox() = default;
 
   AtomicRcBox(const T &value) {
-    ref_count = new AtomicRefCount();
-    data = new T(value);
+    control = new Control();
+    control->data = new T(value);
   }
 
   AtomicRcBox(const AtomicRcBox &other) {
-    ref_count = other.ref_count;
-    data = other.data;
-    if (ref_count) {
-      ref_count->inc();
+    control = other.control;
+    if (control) {
+      control->ref_count.inc();
     }
   }
 
   AtomicRcBox &operator=(const AtomicRcBox &other) {
     if (this != &other) {
-      if (ref_count && ref_count->dec()) {
-        delete data;
-        delete ref_count;
+      if (control && control->ref_count.dec()) {
+        delete control->data;
+        delete control;
       }
-      ref_count = other.ref_count;
-      data = other.data;
-      if (ref_count) {
-        ref_count->inc();
+      control = other.control;
+      if (control) {
+        control->ref_count.inc();
       }
     }
     return *this;
   }
 
   ~AtomicRcBox() {
-    if (ref_count && ref_count->dec()) {
-      delete data;
-      delete ref_count;
+    if (control && control->ref_count.dec()) {
+      delete control->data;
+      delete control;
     }
   }
 
   AtomicRcBox dup() const {
     AtomicRcBox copy;
-    copy.ref_count = new AtomicRefCount();
-    copy.data = new T(*data);
+    copy.control = new Control();
+    if (control && control->data) {
+      copy.control->data = new T(*control->data);
+    }
     return copy;
   }
 
-  T &operator*() {
-    return *data;
+  T *steal() {
+    if (!control) {
+      return nullptr;
+    }
+    T *data = control->data;
+    control->data = nullptr;
+    if (control->ref_count.dec()) {
+      delete control;
+    }
+    control = nullptr;
+    return data;
   }
 
-  T *operator->() {
-    return data;
+  operator T*() {
+    if (control) {
+      return control->data;
+    }
+    return nullptr;
   }
 };
 
