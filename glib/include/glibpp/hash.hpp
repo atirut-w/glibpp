fbf08045d967afd516df8ed1f482fecc2153cce1
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-10-26 16:36:38 +0700

    Stub out HashTable

--- glib/include/glibpp/hash.hpp
+++ glib/include/glibpp/hash.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <glibpp/config.hpp>
+#include <glibpp/refcount.hpp>
 #include <glibpp/types.hpp>
 
 namespace GLib {
@@ -28,5 +29,56 @@ template <> uint hash<int64>(const int64 &v) {
 template <> uint hash<double>(const double &v) {
   return hash<int64>(*reinterpret_cast<const int64 *>(&v));
 }
+
+template <typename K, typename V> class HashTable {
+  struct Control {
+    AtomicRefCount ref_count;
+  };
+
+  Control *control;
+
+public:
+  HashTable() {
+    control = new Control();
+  }
+
+  HashTable(const HashTable &other) {
+    control = other.control;
+    control->ref_count.inc();
+  }
+
+  HashTable &operator=(const HashTable &other) {
+    if (this != &other) {
+      if (control->ref_count.dec()) {
+        delete control;
+      }
+      control = other.control;
+      control->ref_count.inc();
+    }
+    return *this;
+  }
+
+  HashTable(HashTable &&other) noexcept {
+    control = other.control;
+    other.control = nullptr;
+  }
+
+  HashTable &operator=(HashTable &&other) noexcept {
+    if (this != &other) {
+      if (control->ref_count.dec()) {
+        delete control;
+      }
+      control = other.control;
+      other.control = nullptr;
+    }
+    return *this;
+  }
+
+  ~HashTable() {
+    if (control->ref_count.dec()) {
+      delete control;
+    }
+  }
+};
 
 } // namespace GLib
