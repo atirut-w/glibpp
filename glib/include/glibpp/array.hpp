680adc4998f9b79d60a1538adcf911a93ab93605
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-10-29 02:54:50 +0700

    Implement Array functionalities

--- glib/include/glibpp/array.hpp
+++ glib/include/glibpp/array.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <glibpp/config.hpp>x
 #include <glibpp/refcount.hpp>
 #include <glibpp/types.hpp>
 
@@ -14,6 +15,39 @@ template <typename T> class Array {
 
   Control *control = nullptr;
 
+  void maybe_expand(uint len) {
+    // TODO: Overflow check
+
+    uint want_len = control->len + len;
+    if (want_len > control->capacity) {
+      size want_alloc = 1;
+      while (want_alloc < want_len) {
+        want_alloc *= 2;
+      }
+
+      T *new_data = static_cast<T *>(::operator new(sizeof(T) * want_alloc));
+      uint i;
+      try {
+        for (i = 0; i < control->len; i++) {
+          new (new_data + i) T(steal(control->data[i]));
+        }
+      } catch (...) {
+        for (uint j = 0; j < i; j++) {
+          new_data[j].~T();
+        }
+        ::operator delete(new_data);
+        throw;
+      }
+
+      for (uint j = 0; j < control->len; j++) {
+        control->data[j].~T();
+      }
+      ::operator delete(control->data);
+      control->data = new_data;
+      control->capacity = want_alloc;
+    }
+  }
+
 public:
   Array() { control = new Control(); }
 
@@ -26,7 +60,10 @@ public:
   Array &operator=(const Array &other) {
     if (this != &other) {
       if (control && control->refcount.dec()) {
-        delete[] control->data;
+        for (uint i = 0; i < control->len; i++) {
+          control->data[i].~T();
+        }
+        ::operator delete(control->data);
         delete control;
       }
       control = other.control;
@@ -42,7 +79,10 @@ public:
   Array &operator=(Array &&other) noexcept {
     if (this != &other) {
       if (control && control->refcount.dec()) {
-        delete[] control->data;
+        for (uint i = 0; i < control->len; i++) {
+          control->data[i].~T();
+        }
+        ::operator delete(control->data);
         delete control;
       }
       control = other.control;
@@ -53,10 +93,101 @@ public:
 
   ~Array() {
     if (control && control->refcount.dec()) {
-      delete[] control->data;
+      for (uint i = 0; i < control->len; i++) {
+        control->data[i].~T();
+      }
+      ::operator delete(control->data);
       delete control;
     }
   }
+
+  T *data() { return control->data; }
+
+  size len() const { return control->len; }
+
+  Array &append_val(const T &val) { return append_vals(&val, 1); }
+
+  Array &append_vals(const T *vals, uint length) {
+    maybe_expand(length);
+
+    for (uint i = 0; i < length; i++) {
+      new (control->data + control->len + i) T(vals[i]);
+    }
+    control->len += length;
+
+    return *this;
+  }
+
+  Array &prepend_val(const T &val) { return prepend_vals(&val, 1); }
+
+  Array &prepend_vals(const T *vals, uint length) {
+    maybe_expand(length);
+
+    for (uint i = control->len; i > 0; i--) {
+      new (control->data + i + length - 1) T(steal(control->data[i - 1]));
+      control->data[i - 1].~T();
+    }
+
+    for (uint i = 0; i < length; i++) {
+      new (control->data + i) T(vals[i]);
+    }
+    control->len += length;
+
+    return *this;
+  }
+
+  Array &insert_val(uint index, const T &val) {
+    return insert_vals(index, &val, 1);
+  }
+
+  Array &insert_vals(uint index, const T *vals, uint length) {
+    maybe_expand(length);
+
+    for (uint i = control->len; i > index; i--) {
+      new (control->data + i + length - 1) T(steal(control->data[i - 1]));
+      control->data[i - 1].~T();
+    }
+
+    for (uint i = 0; i < length; i++) {
+      new (control->data + index + i) T(vals[i]);
+    }
+    control->len += length;
+
+    return *this;
+  }
+
+  Array &set_size(uint length) {
+    if (length > control->len) {
+      maybe_expand(length - control->len);
+
+      for (uint i = control->len; i < length; i++) {
+        new (control->data + i) T();
+      }
+    } else if (length < control->len) {
+      remove_range(length, control->len - length);
+    }
+    control->len = length;
+
+    return *this;
+  }
+
+  Array &remove_index(uint index) { return remove_range(index, 1); }
+
+  Array &remove_range(uint index, uint length) {
+    for (uint i = index; i < index + length; i++) {
+      control->data[i].~T();
+    }
+
+    for (uint i = index + length; i < control->len; i++) {
+      new (control->data + i - length) T(steal(control->data[i]));
+      control->data[i].~T();
+    }
+    control->len -= length;
+
+    return *this;
+  }
+
+  // TODO: Sort & binary search
 };
 
 } // namespace GLib
