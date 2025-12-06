ab729cb16650003be21099848a6e3f9070bef492
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-12-06 19:46:24 +0700

    Bytes class stub

---
+++ glib/include/glibpp/bytes.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <glibpp/config.hpp>
+
+namespace GLib {
+
+/*
+ * A simple reference counted data type representing an immutable sequence of
+ * zero or more bytes from an unspecified origin.
+ */
+class Bytes {};
+
+} // namespace GLib
