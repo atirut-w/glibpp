367997c35657bab33880c25769e87935b0feac4a
Atirut Wattanamongkol <atirut.wattanamongkol@gmail.com>
2025-12-06 19:57:56 +0700

    Implement basic Bytes semantics

--- glib/include/glibpp/bytes.hpp
+++ glib/include/glibpp/bytes.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "glibpp/refcount.hpp"
 #include <glibpp/config.hpp>
 
 namespace GLib {
@@ -7,6 +8,23 @@ namespace GLib {
  * A simple reference counted data type representing an immutable sequence of
  * zero or more bytes from an unspecified origin.
  */
-class Bytes {};
+class Bytes {
+  struct Control {
+    const void *data = nullptr;
+    gsize size = 0;
+    AtomicRefCount ref_count;
+  };
+
+  Control *control = nullptr;
+
+public:
+  explicit Bytes(const void *data, gsize size);
+  ~Bytes();
+
+  Bytes(const Bytes &other);
+  Bytes &operator=(const Bytes &other);
+  Bytes(Bytes &&other) noexcept;
+  Bytes &operator=(Bytes &&other) noexcept;
+};
 
 } // namespace GLib
